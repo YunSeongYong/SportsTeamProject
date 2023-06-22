5a2f9129a9f190187ed14f2d951ef62cb9856a53
GGG <GGG@DESKTOP-8N3GG4O>
2023-06-22 14:48:37 +0900

    브랜치 수정

--- Sports/src/Main.java
+++ Sports/src/Main.java
@@ -5,8 +5,19 @@ public class Main {
 		System.out.println("Hello World");
 		
 		//윤성용 윤성용(안녕) 박민
-
+  //명완 뺴기
+		
 		System.out.println("유미홍");
 		System.out.println("유미홍2");
 	}
+		//박상현 더하기 인트값 2개 합 반환
+		public int plus(int a, int b) {
+			return a+b;
+		}
+		
+		//명완 뺴기
+		public int minus(int a, int b) {
+			return a-b;
+		}
+	
 }
