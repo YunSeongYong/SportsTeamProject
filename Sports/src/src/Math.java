93cb265c75dad1166ab84eb3e7a93c1080142a0d
윤성용 <xnowhere@naver.com>
2023-06-22 14:41:08 +0900

    plus 추가

--- Sports/src/src/Math.java
+++ Sports/src/src/Math.java
@@ -1,8 +1,14 @@
 package src;
 
 public class Math {
+	public static void plus() {
+		int a = 5;
+		int b = 7;
+		System.out.println(a+b);
+	}
 	public static void main(String[] args) {
 		//윤성용 더하기 인트값 2개 합 반환
+		plus();
 		
 		//정욱진 빼기
 		
