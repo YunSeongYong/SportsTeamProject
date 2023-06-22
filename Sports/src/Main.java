20d23e22bcad674856fa38b06a29ca9db30ccf3f
GGG <GGG@DESKTOP-8N3GG4O>
2023-06-22 12:54:28 +0900

    변경

--- Sports/src/Main.java
+++ Sports/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		
-		//윤성용
+		//윤성용 유미홍
 
 		System.out.println("유미홍");
 		System.out.println("유미홍2");
